f66ede7db0e9b2755950165d15fb6ef4e131b9b9
AhsanBuksh0 <138068406+AhsanBuksh0@users.noreply.github.com>
2023-08-02 12:18:20 +1200

    Create v3.py

---
+++ v3.py
@@ -0,0 +1,19 @@
+# intialise Answer var and assign value
+intAnswer = 123
+# intialise Guess var and assign value
+intGuess = 1
+
+# start WHILE loop
+while intGuess != 0:
+
+   # get the input from user to assign to variable
+   intGuess = int(input("Enter your Guess or 0 to Exit:"))
+
+   #compare variable and print outcome
+   if intAnswer < intGuess:
+    print("Your Guess", intGuess, "is TOO HIGH.")
+   elif intAnswer > intGuess:
+    print("Your Guess", intGuess, "is TOO LOW.")
+   else:
+    print("Your Guess", intGuess, "is CORRECT.")
+    break
